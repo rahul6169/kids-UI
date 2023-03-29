ea24d3fd105f06fd77abd0dc311d05cdad963b39
RAHUL R B <rahulbalakrishnan6169@gmail.com>
2023-03-29 13:12:41 +0530

    timeline page worked

--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -1,15 +1,60 @@
 const TimeLine = () => {
   return (
-    <div className="row  mb-5 pb-5">
-      <div className="col-lg-6 col-md-6 pe-5 col-sm-12 text-center">
-        <h3 className="timeline-year">2012</h3>
-        <h3 className="star-text-points">Outdoor Activity</h3>
-        <p className="lorem-ipsum-text lorem-timeline-text ">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
+    <>
+      <div className="row mb-5 pb-5 align-row-timeline  align-timeline-row">
+        <div className="col-lg-6 col-md-6 pe-5 col-sm-12 mt-4 text-end">
+          <h3 className="timeline-year">2012</h3>
+          <h3 className="star-text-points">Outdoor Activity</h3>
+          <p className="lorem-ipsum-text lorem-timeline-text ps-5 text-end">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <div className="col-lg-6 text-start ps-5">
+          <img className="image-timeline" src="../assets/timeline1.jpg"></img>
+        </div>
       </div>
-      <div className="col-lg-6"></div>
-    </div>
+
+      <div className="row mb-5 pb-5 align-row-timeline  align-timeline-row">
+        <div className="col-lg-6 text-end pe-5">
+          <img className="image-timeline" src="../assets/timeline1.jpg"></img>
+        </div>
+
+        <div className="col-lg-6 col-md-6 ps-5 col-sm-12 text-start">
+          <h3 className="timeline-year">2011</h3>
+          <h3 className="star-text-points">Outdoor Activity</h3>
+          <p className="lorem-ipsum-text lorem-timeline-text-left  text-start">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+
+      <div className="row mb-5 pb-5 align-row-timeline  align-timeline-row">
+        <div className="col-lg-6 col-md-6 pe-5 col-sm-12 mt-4 text-end">
+          <h3 className="timeline-year">2010</h3>
+          <h3 className="star-text-points">Outdoor Activity</h3>
+          <p className="lorem-ipsum-text lorem-timeline-text ps-5 text-end">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <div className="col-lg-6 ps-5 text-start">
+          <img className="image-timeline" src="../assets/timeline1.jpg"></img>
+        </div>
+      </div>
+
+      <div className="row mb-5 pb-5 align-row-timeline  align-timeline-row">
+        <div className="col-lg-6 text-end pe-5">
+          <img className="image-timeline" src="../assets/timeline1.jpg"></img>
+        </div>
+
+        <div className="col-lg-6 col-md-6 ps-5 col-sm-12 text-start">
+          <h3 className="timeline-year">2009</h3>
+          <h3 className="star-text-points">Outdoor Activity</h3>
+          <p className="lorem-ipsum-text lorem-timeline-text-left  text-start">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 export { TimeLine };
